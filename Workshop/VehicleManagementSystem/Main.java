8ba2fc7cccdd5769edd1b493cb5d5ea84d1f4658
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2025-07-07 18:45:05 +0530

    Delete Workshop directory

--- Workshop/VehicleManagementSystem/Main.java
+++
@@ -1,8 +0,0 @@
-package VehicleManagementSystem;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehicle car = new Car("Tata", "Nixon" , "petrol",123);
-        car.printServiceDetails();
-    }
-}
