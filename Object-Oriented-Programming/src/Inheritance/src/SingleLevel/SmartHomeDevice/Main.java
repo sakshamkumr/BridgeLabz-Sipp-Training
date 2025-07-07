a54c1de7b154fcdfb3c4541ee87c78dcfe491c01
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2025-07-07 18:44:55 +0530

    Delete Object-Oriented-Programming directory

--- Object-Oriented-Programming/src/Inheritance/src/SingleLevel/SmartHomeDevice/Main.java
+++
@@ -1,8 +0,0 @@
-package SingleLevel.SmartHomeDevice;
-
-public class Main {
-    public static void main(String[] args) {
-        Thermostat myThermostat = new Thermostat("THERMO123", true, 22.5);
-        myThermostat.displayStatus();
-    }
-}
